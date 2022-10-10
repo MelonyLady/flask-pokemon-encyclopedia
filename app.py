2ccc7fa719182f9b446a462de00e12d600269b3b
Sydney Humayun <psychdreamer@hotmail.co.uk>
2022-10-10 21:53:35 +0100

    add pokeomon data html page

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template
 from helpers import get_pokemon_by_name, get_random_pokemon_list
 
@@ -8,18 +9,13 @@ app = Flask(__name__)
 @app.get("/")
 def pokemon_list():
     random_pokemon = get_random_pokemon_list()
-    # return ', '.join([pokemon['name'] for pokemon in get_random_pokemon_list()]).title()
     return render_template("pokemon_list.html", random_pokemon=random_pokemon)
 
 
 @app.get("/<pokemon_name>")
 def pokemon_data(pokemon_name):
     pokemon = get_pokemon_by_name(pokemon_name)
-    return f"This is {pokemon['name'].capitalize()}.\n" \
-           f"Height: {pokemon['height']}.\n" \
-           f"Weight: {pokemon['weight']}.\n" \
-           f"Base experience: {pokemon['base_experience']}.\n" \
-           f"Type(s): {' and '.join(type_info['type']['name'] for type_info in pokemon['types'])}"
+    return render_template("pokemon_data.html", pokemon=pokemon)
 
 
 if __name__ == "__main__":
